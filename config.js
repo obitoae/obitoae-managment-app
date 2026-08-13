403fdf7d1ee358033dfec512c4de9e7b89872a85
obitoae <alexisvalentinrcc@gmail.com>
2026-08-13 10:45:22 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Rellena estos dos valores con los de tu proyecto de Supabase:
-// Panel de Supabase → Settings (⚙️) → API → "Project URL" y "anon public" key.
-export const SUPABASE_URL = "PON_AQUI_TU_PROJECT_URL";
-export const SUPABASE_ANON_KEY = "PON_AQUI_TU_ANON_PUBLIC_KEY";
+// Panel de Supabase → Settings (⚙️) → API Keys → "Project URL" y "Publishable key".
+export const SUPABASE_URL = "https://fnwezdfgypocwgngxjxn.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_NDpdN6mCEHFLjBaNAhVQBw_4L84i8Ia";
